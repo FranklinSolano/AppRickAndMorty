a2b86cb9362141898e053b8eb932baea6dd42d4c
FranklinSolano <100084667+FranklinSolano@users.noreply.github.com>
2025-10-24 19:45:53 -0300

    Initial Commit

---
+++ AplicativoRickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AplicativoRickAndMorty
+//
+//  Created by Franklin  Stilhano Solano on 24/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
